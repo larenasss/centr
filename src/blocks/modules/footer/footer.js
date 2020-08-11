5d39b47b8b5f08e9ce34010d24d3c2b694735b76
larenasss <ab-vis@mail.ru>
2020-08-11 21:05:32 +0700

    test

--- src/blocks/modules/footer/footer.js
+++ src/blocks/modules/footer/footer.js
@@ -1,15 +1,15 @@
-const menuList = document.querySelectorAll('.footer__list');
-const itemClickShowList = document.querySelectorAll('.footer__block-title');
+const menuList = document.querySelectorAll(".footer__list");
+const itemClickShowList = document.querySelectorAll(".footer__block-title");
 
 for (let i = 0; i < itemClickShowList.length; i++) {
 
-  itemClickShowList[i].addEventListener('click', () => {
-    if(menuList[i].classList.contains('active')) {
-      menuList[i].classList.remove('active');
-      itemClickShowList[i].classList.remove('active');
-    } else {
-      menuList[i].classList.add('active');
-      itemClickShowList[i].classList.add('active');
-    }
-  })
+    itemClickShowList[i].addEventListener("click", () => {
+        if(menuList[i].classList.contains("active")) {
+            menuList[i].classList.remove("active");
+            itemClickShowList[i].classList.remove("active");
+        } else {
+            menuList[i].classList.add("active");
+            itemClickShowList[i].classList.add("active");
+        }
+    });
 }
